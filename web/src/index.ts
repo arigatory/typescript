3e97fe255ca0ceefb31ac1986aae1515eacdb748
Ivan MSI <arigatory@gmail.com>
2022-08-31 12:17:00 +0300

    add event system start

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,7 @@
-console.log(1);
+import { User } from './models/User';
+const user = new User({ name: 'John', age: 22 });
+
+user.set({name: 'Ivan', age: 22});
+user.set({age:100});
+console.log(user.get('name'));
+console.log(user.get('age'));
---
+++ web/src/models/User.ts
@@ -0,0 +1,24 @@
+interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => {};
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    
+  }
+}
+
+new User({ name: 'Ivan', age: 30 });
