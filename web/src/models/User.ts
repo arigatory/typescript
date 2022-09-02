8f3ce75e74d2ae12c5153584d2c0e58f9ff5afd0
Ivan MSI <arigatory@gmail.com>
2022-09-02 04:10:55 +0300

    add event system for user

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -3,9 +3,11 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => {};
+type Callback = () => void;
 
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -17,8 +19,19 @@ export class User {
   }
 
   on(eventName: string, callback: Callback): void {
-    
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }
-
-new User({ name: 'Ivan', age: 30 });
