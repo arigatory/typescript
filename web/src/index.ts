accd7eefbdd9cd853c3bf43ec92be3a2485081fd
Ivan MSI <arigatory@gmail.com>
2022-09-04 21:57:33 +0300

    all framework done

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,16 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
+import { User} from './models/User';
 
-const userForm = new UserForm(
-  document.getElementById('root')
-);
 
-userForm.render();
+const user = User.buildUser({ name: 'Ivan', age: 36 });
+
+const root = document.getElementById('root');
+
+if (root) {
+  const userEdit = new UserEdit(root, user);
+
+  userEdit.render();
+  console.log(userEdit);
+} else {
+  throw new Error('No root!');
+}
--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,21 +1,41 @@
-export class UserForm {
-  constructor(public parent: Element | null) {}
+import { User, UserProps } from '../models/User';
+import { View } from './View';
+export class UserForm extends View<User, UserProps> {
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
+    };
+  }
+
+  onSaveClick = ():void => {
+    this.model.save();
+  };
+
+  onSetNameClick = (): void => {
+    if (this.parent === null)
+      return;
+    const input = this.parent.querySelector('input');
+
+    if (input) {
+      const name = input.value;
+      this.model.set({name});
+    }
+  };
+
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   template(): string {
     return `
       <div>
-        <h1>User Form</h1>
-        <input />
-        <button>Click me</button>
+        <input placeholder="${this.model.get('name')}" />
+        <button class="set-name">Change name</button>
+        <button class="set-age">Set random age</button>
+        <button class="save-model">Save User</button>
       </div>
     `;
-  }
-
-  render(): void {
-    const templateElement = document.createElement('template');
-    templateElement.innerHTML = this.template();
-
-    if (this.parent)
-      this.parent.append(templateElement.content);
   }
 }
