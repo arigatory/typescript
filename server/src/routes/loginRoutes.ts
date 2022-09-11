4ee0536fded6a71a1cc52ec1f9d5fac1cea2c89e
Ivan MSI <arigatory@gmail.com>
2022-09-11 09:10:50 +0300

    dealing with type def

---
+++ server/src/routes/loginRoutes.ts
@@ -0,0 +1,29 @@
+import { Router, Request, Response } from 'express';
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
+const router = Router();
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input type="email" name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+  res.send(email.toUpperCase());
+});
+export { router };
